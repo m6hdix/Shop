8155de33d7f1d1e146a26eba1d2e61f00850b4e5
m6hdix <m6hdixx@gmail.com>
2023-05-10 17:54:57 +0430

    🔨:add loding

--- src/Components/store.jsx
+++ src/Components/store.jsx
@@ -6,14 +6,23 @@ export default function StorePage() {
   const products = useContext(ContextProvider);
 
   return (
-    <div className="flex items-center justify-center ">
-      <div
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 py-4 place-content-center"
-      >
-        {products.map((product) => (
-          <ProductCard Product={product} key={product.id} />
-        ))}
-      </div>
+    <div className={`flex items-center justify-center ${products.length <= 0 ?"h-screen":""}`}>
+      {products.length <= 0 ? (
+        <div className="mockup-code  bg-primary text-primary-content transition-transform">
+          <pre data-prefix="$">
+            <code>Loading...</code>
+          </pre>
+          <pre data-prefix="$">
+            <code>Please wait for the data to be delivered</code>
+          </pre>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 py-4 place-content-center">
+          {products.map((product) => (
+            <ProductCard Product={product} key={product.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
